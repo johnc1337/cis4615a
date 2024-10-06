e0a7ab7dbc2ea6814a77475d72a5640d6a10bdc7
johnc1337 <demonoidjc@gmail.com>
2024-10-06 17:08:55 -0400

    Correct R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,4 +1,7 @@
 public void deleteFile() {
     File someFile = new File("someFileName.txt");
-    someFile.delete();
+    if (!someFile.delete()) {
+        // Handle failure to delete the file
+        System.out.println("Failed to delete the file.");
+    }
 }
