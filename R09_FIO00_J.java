4fc974c3159cfc4d33a0bf8d7ee86dcfd772aa3d
johnc1337 <demonoidjc@gmail.com>
2024-10-06 17:26:30 -0400

    Correct R09_FIO00_J.java

--- R09_FIO00_J.java
+++ R09_FIO00_J.java
@@ -1,5 +1,7 @@
 public void writeFile(String data) throws IOException {
-    FileWriter writer = new FileWriter("output.txt");
-    writer.write(data);
-    // File is not closed
+    try (FileWriter writer = new FileWriter("output.txt")) {
+        writer.write(data);
+    } catch (IOException e) {
+        System.err.println("An error occurred: " + e.getMessage());
+    }
 }
