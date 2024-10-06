26e52f9df0a0fab35e80d0b0b85774e0d699632d
johnc1337 <demonoidjc@gmail.com>
2024-10-06 17:20:27 -0400

    Create R07_ERR00_J.java

---
+++ R07_ERR00_J.java
@@ -0,0 +1,8 @@
+public void readFile() {
+    try {
+        FileReader reader = new FileReader("test.txt");
+        // Read from the file
+    } catch (IOException e) {
+        // Suppressing the exception
+    }
+}
