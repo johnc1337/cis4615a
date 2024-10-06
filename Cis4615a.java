56831067e3d78280d25428416bbb95fb905f39d8
johnc1337 <demonoidjc@gmail.com>
2024-10-06 16:54:59 -0400

    Add cis4615a.java

---
+++ Cis4615a.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *  Compilation:  javac Cis4615a.java
+ *  Execution:    java Cis4615a
+ *
+ *  Prints 
+ *    "Hello, World - from Jonathan Carver".
+ *  By tradition, this is everyone's first program.
+ *
+ * Revision History:
+ *   % java Cis4615a
+ *  Hello, World - from Jonathan Carver
+ *  %
+ *
+ ******************************************************************************/
+
+public class Cis4615a {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World - from Jonathan Carver");
+    }
+}
